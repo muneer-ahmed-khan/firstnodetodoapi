d158ee45474c6628a37a82d000f371bd1d1b9581
muneer khan <muneerkhan31886@gmail.com>
2020-09-10 19:52:27 +0500

    test post todos route

---
+++ server/test/server.test.js
@@ -0,0 +1,55 @@
+const expect = require('expect')
+const supertest = require('supertest')
+const request =  require('http')
+
+const {app} = require('./../server')
+const {Todo} = require('./../models/todo')
+
+beforeEach((done) => {
+  Todo.remove({}).then(() => {
+    done();
+  })
+})
+
+describe('POST /todos', () => {
+  it('should create a new todo.', (done) => {
+    var text = 'Test todo text'
+
+    request(app)
+    .post('/todos')
+    .send({text})
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.text).toBe(text);
+    })
+    .end((err, res) => {
+      if (err) {
+        done('my friend error comes  here')
+      }
+
+      Todo.find().then((todos) => {
+        expect(todos.length).toBe(1);
+        expect(todo[0].text).toBe(text);
+        done();
+      }).catch((e) => done('is there any errir'))
+
+    })
+  })
+
+  it('should not create todo with invalid body data', (done) => {
+    request(app)
+    .post('/todos')
+    .send({})
+    .expect(400)
+    .end((err, res) => {
+      if (err) {
+        return done(err)
+      }
+
+      Todo.find().then((todos) => {
+        expect(todos.length).toBe(0);
+        done();
+      }).catch((e) => done(e))
+    })
+  })
+})
